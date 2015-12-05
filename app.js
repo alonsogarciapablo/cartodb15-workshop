84e8cb0cb7fc0e94530ec6bd1bd2264c92e2d450
Pablo Alonso Garcia <pablo@cartodb.com>
2015-12-05 20:32:20 +0100

    Added a couple of Widgets. Not a very MVC approach.

---
+++ app.js
@@ -0,0 +1,71 @@
+// TODO:
+//  - Add title to the dashboard
+//  - Customize infowindow
+//  - Add widget that gets some data from SQL API and filters by category
+
+var main = function() {
+  var vizjson = 'https://cartodb15.cartodb.com/api/v2/viz/66bffecc-99e2-11e5-82c2-0ecd1babdde5/viz.json';
+  cartodb.createVis('map', vizjson, {
+    shareable: false,
+    title: false,
+    description: false,
+    search: false,
+    tiles_loader: true
+  })
+  .done(onVisCreated)
+  .error(onError);
+};
+
+var onVisCreated = function(vis, layers) {
+  var editorLayers = layers[1];
+  var airbnbListings = editorLayers.getSubLayer(3);
+  var filters = document.querySelectorAll('.js-filter'), i;
+
+  for (i = 0; i < filters.length; ++i) {
+    filters[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      onFilterClicked(event.target, airbnbListings);
+    }, false);
+  }
+};
+
+var onFilterClicked = function(filter, sublayer) {
+  applyFilter(filter);
+  updateSublayerSQL(sublayer);
+};
+
+var applyFilter = function(filter) {
+  var widget = filter.closest('.js-widget');
+
+  var filters = widget.querySelectorAll('.js-filter');
+  for (i = 0; i < filters.length; ++i) {
+    filters[i].dataset.active = "false";
+    filters[i].classList.remove('is-active');
+  }
+  if (filter.dataset.queryCondition) {
+    filter.dataset.active = "true";
+    filter.classList.add('is-active');
+  }
+};
+
+var updateSublayerSQL = function(sublayer) {
+  var originalSQL = 'SELECT * FROM airbnb_listings';
+  var conditions = [];
+  var activeFilters = document.querySelectorAll('.js-filter[data-active="true"]');
+
+  for (i = 0; i < activeFilters.length; ++i) {
+    conditions.push(activeFilters[i].dataset.queryCondition);
+  }
+  if (conditions.length) {
+    var newQuery = originalSQL + ' WHERE ' + conditions.join(' AND ');
+  } else {
+    var newQuery = originalSQL;
+  }
+  sublayer.setSQL(newQuery);
+}
+
+var onError = function(err) {
+  alert('error!');
+};
+
+window.onload = main;
