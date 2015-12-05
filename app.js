0443cc3dec8eba0c5a3d8d39553895df253ac9c9
Pablo Alonso Garcia <pablo@cartodb.com>
2015-12-05 21:34:05 +0100

    Little refactoring here and there

--- app.js
+++ app.js
@@ -6,60 +6,80 @@
 // .done(function(data){
 //   alert(data.rows[0].price);
 // });
-
+//  - Widget to filter by line name
 var main = function() {
   var vizjson = 'https://cartodb15.cartodb.com/api/v2/viz/66bffecc-99e2-11e5-82c2-0ecd1babdde5/viz.json';
-  cartodb.createVis('map', vizjson, {
+  var options = {
     shareable: false,
     title: false,
     description: false,
     search: false,
     tiles_loader: true
-  })
+  };
+  cartodb.createVis('map', vizjson, options)
   .done(onVisCreated)
   .error(onError);
 };
 
 var onVisCreated = function(vis, layers) {
-  var editorLayers = layers[1];
-  var airbnbListings = editorLayers.getSubLayer(3);
-  var filters = document.querySelectorAll('.js-filter');
+  var sublayer = layers[1].getSubLayer(3);
+  var activeFilters = [];
+  var widgets = document.querySelectorAll('.js-widget');
+  for (i = 0; i < widgets.length; i++) {
+    widget = widgets[i];
 
-  for (i = 0; i < filters.length; ++i) {
-    filters[i].addEventListener('click', function(event) {
-      event.preventDefault();
-      onFilterClicked(event.target, airbnbListings);
-    }, false);
+    var filters = widget.querySelectorAll('.js-filter');
+    for (j = 0; j < filters.length; j++) {
+      filter = filters[j];
+
+      bindClickEvents(widget, filter, sublayer);
+    }
   }
 };
 
-var onFilterClicked = function(filter, sublayer) {
-  applyFilter(filter);
-  updateSublayerSQL(sublayer);
+var bindClickEvents = function(widget, filter, sublayer) {
+  filter.addEventListener('click', function(event) {
+    event.preventDefault();
+    applyFilter(widget, filter);
+    reloadWidget(widget);
+
+    // TODO: Keep track of active filters somewhere
+    var activeFilters = document.querySelectorAll('.js-filter[data-active="true"]');
+    updateSublayerSQL(sublayer, activeFilters);
+  }, false);
 };
 
-var applyFilter = function(filter) {
-  var widget = filter.closest('.js-widget');
+var applyFilter = function(widget, filter) {
   var isFilterActive = filter.dataset.active === "true";
 
   var filters = widget.querySelectorAll('.js-filter');
-  for (i = 0; i < filters.length; ++i) {
-    filters[i].dataset.active = "false";
-    filters[i].classList.remove('is-active');
+  for (i = 0; i < filters.length; i++) {
+    if (filters[i].dataset.queryCondition) {
+      filters[i].dataset.active = "false";
+    }
   }
-
   if (!isFilterActive && filter.dataset.queryCondition) {
     filter.dataset.active = "true";
-    filter.classList.add('is-active');
   }
 };
 
-var updateSublayerSQL = function(sublayer) {
+var reloadWidget = function(widget) {
+  var filters = widget.querySelectorAll('.js-filter');
+  for (i = 0; i < filters.length; i++) {
+    var filter = filters[i];
+    if (filter.dataset.active === "true") {
+      filter.classList.add('is-active');
+    } else {
+      filter.classList.remove('is-active');
+    }
+  }
+};
+
+var updateSublayerSQL = function(sublayer, activeFilters) {
   var originalSQL = 'SELECT * FROM airbnb_listings';
   var conditions = [];
-  var activeFilters = document.querySelectorAll('.js-filter[data-active="true"]');
 
-  for (i = 0; i < activeFilters.length; ++i) {
+  for (i = 0; i < activeFilters.length; i++) {
     conditions.push(activeFilters[i].dataset.queryCondition);
   }
   if (conditions.length) {
